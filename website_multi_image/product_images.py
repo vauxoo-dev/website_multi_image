09b125a0e560f41331098956969ea43135173b70
oalca <oszckar@gmail.com>
2015-11-21 12:15:23 -0600

    [FIX] Lint

--- website_multi_image/product_images.py
+++ website_multi_image/product_images.py
@@ -1,27 +1,14 @@
+# coding: utf-8
 from openerp import models, fields, api
 
 
-# class product_image(models.Model):
-#     _name = 'product.image'
-#     _order = 'sequence, id DESC'
-
-#     name = fields.Char('Name')
-#     description = fields.Text('Description')
-#     sequence = fields.Integer('Sequence')
-#     image_alt = fields.Text('Image Label')
-#     image = fields.Binary('Image')
-#     image_small = fields.Binary('Small Image')
-#     product_tmpl_id = fields.Many2one('product.template', 'Product', select=True)
-#     from_main_image = fields.Boolean('From Main Image', default=False)
-
-
-class ir_attachment(models.Model):
+class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
 
     image_id = fields.Many2one('product.template', string='Images')
 
 
-class product_template(models.Model):
+class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     images = fields.One2many('ir.attachment', 'image_id',
